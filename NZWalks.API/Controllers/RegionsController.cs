8b084689f1cc86637560bff0c073c9f8cf4b5a8a
AnoopChaleppady <Anoopchaleppadi46@gmail.com>
2023-07-19 01:03:43 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTOs;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get Data From Database -Domain Models
+            var regionsDomain = dbContext.Regions.ToList();
+            //Map Domain Models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Name = regionDomain.Name,
+                    Code = regionDomain.Code,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+
+            
+
+            //Return DTOs to Client
+             return Ok(regionsDto);
+        }
+
+        //get single region(get region by id)
+        [HttpGet("{id:Guid}")]
+        public IActionResult GetById(Guid id)//because ur id is guid
+        {
+            //var region = dbContext.regions.Find(id);
+            
+
+
+            //we can use FirstOrDefault instead of .Find so wecan pass any id we want if we add Find(id) we must pass id what is there in db
+            //find method only takes primary key
+            //Get Region Domain model from Databaes
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            // in this method we can use code name anything is there it must there in route also
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            //map or convert domain model to region dto
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            //return DTO back to client
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map or Conver DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to Create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            // Map Domain model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
+
+        // Update region
+        // PUT
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDtocs updateRegionRequestDtocs)
+        {
+            // Check  if region exists
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Map DTO to Domain model
+            regionDomainModel.Code = updateRegionRequestDtocs.Code;
+            regionDomainModel.Name = updateRegionRequestDtocs.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDtocs.RegionImageUrl;
+
+            dbContext.SaveChanges();
+
+            // Conver Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok();
+        }
+
+        // Delete Region
+        // DELETE:
+        [HttpDelete("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Delete region
+            dbContext.Regions.Remove(regionDomainModel);
+            dbContext.SaveChanges();
+
+            // return deleted Region back
+            // Map Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,22 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LegthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+
+        //one to one relationship btwn walk nd diffi walk nd regi
+        public Guid DifficultyId { get; set; }//for relation ship
+
+        public Guid RegionId { get; set; }
+
+        // Navigation propertie
+        public Difficulty Difficulty { get; set; }// walk will have difficulty linking walk to difficulty
+        public Region Region { get; set; }
+
+    }
+}
