9cc5e5624919059da47e6e6f078a07951c865b82
AnoopChaleppady <Anoopchaleppadi46@gmail.com>
2023-07-19 20:44:48 +0530

    Added a new folder called Mapper and installed automapper dependencies

---
+++ NZWalks.API/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTOs;
+
+namespace NZWalks.API.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Region, RegionDto>().ReverseMap();
+            CreateMap<AddRegionRequestDto, Region>().ReverseMap();
+            CreateMap<UpdateRegionRequestDtocs, Region>().ReverseMap();
+            
+        }
+    }
+
+
+    
+
+        
+}
